826f34d7dde76a866d26113a8b15d15de597092f
Ty <ty@pre-alpha.com>
2015-04-16 18:04:23 -0700

    Fix ns to ms conversion
    Well this is embarrassing for me!

--- src/lib.rs
+++ src/lib.rs
@@ -121,7 +121,7 @@ pub fn precise_time_s() -> f64 {
  * in milliseconds since an unspecified epoch.
  */
 pub fn precise_time_ms() -> u64 {
-    return precise_time_ns() / 1000;
+    return precise_time_ns() / 1000000;
 }
 
 // Computes (value*numer)/denom without overflow, as long as both
